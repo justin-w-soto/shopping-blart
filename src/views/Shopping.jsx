c4b38e9381421ab2b11ee86bf83bc0056274f9a3
Justin W Soto <soto@pdx.edu>
2021-12-15 16:57:35 -0800

    added pseudocode

---
+++ src/views/Shopping.jsx
@@ -0,0 +1,9 @@
+import { useReducer } from 'react'
+
+// create reducer function to handle state flux
+// use switch inside reducer
+// use dispatch to update state
+// set initial value for useReducer
+// include add edit delete functionality
+// behavior test all 
+
