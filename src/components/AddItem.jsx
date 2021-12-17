a036d6f5eb66b166a8c88b3707b61b4e7a367261
Justin W Soto <soto@pdx.edu>
2021-12-17 10:57:10 -0800

    provided state and functional btn to additem

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
-export const AddItem = () => {
+export const AddItem = ({ onAddItem }) => {
+    const [text, setText] = useState('');
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setText('');
+      onAddItem(text);
+    };
     return (
-        <form>
-            <input type="text" placeholder='enter text'/>
-            <button type='submit'>Add</button>        </form>
+        <form onSubmit={handleSubmit} className="form">
+        <input
+          className="add-item"
+          value={text}
+          placeholder="Add an Item"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit" className="submit-btn">
+          ADD
+        </button>
+      </form>
     )
 }
